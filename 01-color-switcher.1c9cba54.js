52252955546833ca349a9a345c0ac81eabdf9405
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2023-09-04 19:06:17 +0000

    Deploying to gh-pages from @ OksanaLukashyk/goit-js-hw-09@9f9651404496f6e714750152d7f93a6d54538b11 🚀

---
+++ 01-color-switcher.1c9cba54.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=document.body;e.addEventListener("click",(function(){n.disabled=!1,e.disabled=!0,t=setInterval((function(){return a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.1c9cba54.js.map
